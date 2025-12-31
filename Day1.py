49fc25c61df584fd5c22c161e57801d4f8d36f8e
kshitij7k <kshitijparmeshwar73@gmail.com>
2025-12-31 20:37:59 +0530

    Add files via upload

---
+++ Day1.py
@@ -0,0 +1,13 @@
+print(2+3) #add
+print(3-2) #sub
+print(3**2) #power
+print(3*2) #multiplication
+print(3%2) #mod
+print(3/2) #div 
+print(3//2) #floor div
+
+print(type(10)) 
+print(type(10.4))
+print(type("hello bro"))
+print(type([10,"apple",99,"pen"]))
+print(type({1:"carrot",2:"cherry"}))
